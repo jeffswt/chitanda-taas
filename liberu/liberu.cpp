c48dc77b44f80516eb93988072bf3eada439f7af
Geoffrey Tang <jeffswt@users.noreply.github.com>
2021-06-24 16:45:55 +0800

    refactor!: liberu structure modification

--- liberu/liberu.cpp
+++ liberu/liberu.cpp
@@ -1,192 +1,375 @@
 
-#include <tfhe/tfhe.h>
 #include <tfhe/tfhe_io.h>
-
 #include <openssl/rand.h>
+#include "liberu.h"
 
-#include <iostream>
-#include <memory>
 #include <sstream>
 
-class EruException : public std::exception {
-private:
-    std::string _what;
-public:
-    EruException(std::string what) : _what(what) {}
-    const char* what() {
-        return this->_what.c_str();
-    }
-};
 
-typedef std::string EruBytes;
-
-EruBytes _extract_sstream_data(std::stringstream &stream) {
-    EruBytes result;
+EruData _extract_sstream_data(std::stringstream &stream) {
+    EruData result;
     int ch;
     while ((ch = stream.get()) != EOF)
         result += (char)ch;
     return result;
 }
 
-class TFheGateBootstrappingParameterSetDeleter {
+class _TFheGateBootstrappingParameterSetDeleter {
 public:
     void operator() (TFheGateBootstrappingParameterSet *ptr) {
         delete_gate_bootstrapping_parameters(ptr);
     }
 };
 
-class TFheGateBootstrappingSecretKeySetDeleter {
+class _TFheGateBootstrappingSecretKeySetDeleter {
 public:
     void operator() (TFheGateBootstrappingSecretKeySet *ptr) {
         delete_gate_bootstrapping_secret_keyset(ptr);
     }
 };
 
-class EruKeys {
-protected:
-    std::shared_ptr<TFheGateBootstrappingParameterSet> _bootstrap_params;
-    std::shared_ptr<TFheGateBootstrappingSecretKeySet> _secret_key;
-    std::shared_ptr<TFheGateBootstrappingCloudKeySet> _cloud_key;
+class _TFheGateBootstrappingCloudKeySetDeleter {
 public:
-    EruKeys() : _bootstrap_params(nullptr), _secret_key(nullptr),
-        _cloud_key(nullptr) {}
-    EruKeys(std::shared_ptr<TFheGateBootstrappingParameterSet> bp,
-        std::shared_ptr<TFheGateBootstrappingSecretKeySet> sk) :
-        _bootstrap_params(bp), _secret_key(sk), _cloud_key(nullptr) {}
-    EruKeys(std::shared_ptr<TFheGateBootstrappingParameterSet> bp,
-        std::shared_ptr<TFheGateBootstrappingCloudKeySet> ck) :
-        _bootstrap_params(bp), _secret_key(nullptr), _cloud_key(ck) {}
-    EruKeys(const EruKeys &other) :
-        _bootstrap_params(other._bootstrap_params),
-        _secret_key(other._secret_key) {}
-    EruBytes secret_key() {
-        std::stringstream stream;
-        export_tfheGateBootstrappingSecretKeySet_toStream(
-            stream, this->get_secret_key());
-        return _extract_sstream_data(stream);
-    }
-    EruBytes cloud_key() {
-        std::stringstream stream;
-        export_tfheGateBootstrappingCloudKeySet_toStream(
-            stream, this->get_cloud_key());
-        return _extract_sstream_data(stream);
-    }
-    const TFheGateBootstrappingSecretKeySet* get_secret_key() {
-        return this->_secret_key.get();
-    }
-    const TFheGateBootstrappingCloudKeySet* get_cloud_key() {
-        const TFheGateBootstrappingCloudKeySet *ptr;
-        if (_secret_key != nullptr)
-            ptr = &this->_secret_key.get()->cloud;
-        else
-            ptr = this->_cloud_key.get();
-        return ptr;
+    void operator() (TFheGateBootstrappingCloudKeySet *ptr) {
+        delete_gate_bootstrapping_cloud_keyset(ptr);
     }
 };
 
-class EruEnv {
-protected:
-    int minimum_lambda = 110;
-    std::shared_ptr<TFheGateBootstrappingParameterSet> _bootstrap_params;
-    EruKeys _keys;
-public:
-    EruEnv() {
-        this->_bootstrap_params = std::shared_ptr
-            <TFheGateBootstrappingParameterSet>(
-            new_default_gate_bootstrapping_parameters(minimum_lambda),
-            TFheGateBootstrappingParameterSetDeleter()
-        );
-    }
-    void seed() {
-        // Resets seed of Eru environment using system random engine.
-        // This function is cryptographically secure.
-        int size = minimum_lambda * 2 / 32 + 1;  // how many uint32_t nums
-        uint32_t *buffer = new uint32_t[size];
-        if (RAND_bytes((unsigned char*)buffer, size * 4) != 1)
-            throw EruException("cannot generate seed");
-        tfhe_random_generator_setSeed(buffer, size);
-    }
-    void make_keys() {
-        // Generates brand new secret key (and corresponding cloud keys).
-        this->seed();
-        TFheGateBootstrappingSecretKeySet *key;
-        key = new_random_gate_bootstrapping_secret_keyset(
-            this->_bootstrap_params.get());
-        auto ptr = std::shared_ptr<TFheGateBootstrappingSecretKeySet>(key,
-            TFheGateBootstrappingSecretKeySetDeleter());
-        this->_keys = EruKeys(this->_bootstrap_params, ptr);
-    }
-    void set_keys(EruKeys keys) {
-        this->_keys = keys;
-    }
-    EruKeys get_keys() {
-        return this->_keys;
-    }
-    // creation and destruction of values
-    LweSample* create(int size) {
-        return new_gate_bootstrapping_ciphertext_array(size,
-            this->_bootstrap_params.get());
-    }
-    void destroy(LweSample *a, int size) {
-        delete_gate_bootstrapping_ciphertext_array(size, a);
-    }
-    // copy of homomorphic functions running in this environment
-    #define _basic_unary_op(defined_name, tfhe_func_name)                     \
-    void defined_name(LweSample *r, const LweSample *a) {                     \
-        tfhe_func_name(r, a, this->_keys.get_cloud_key());                    \
-    }
-    #define _basic_binary_op(defined_name, tfhe_func_name)                    \
-    void defined_name(LweSample *r, const LweSample *a, const LweSample *b) { \
-        tfhe_func_name(r, a, b, this->_keys.get_cloud_key());                 \
-    }
-    _basic_unary_op(ldup, bootsCOPY)  // r = a
-    _basic_unary_op(lnot, bootsNOT)  // r = !a
-    _basic_binary_op(land, bootsAND)  // r =  a && b
-    _basic_binary_op(lor, bootsOR)  // r = a || b
-    _basic_binary_op(lnand, bootsNAND)  // r = !(a && b)
-    _basic_binary_op(lnor, bootsNOR)  // r = !(a || b)
-    _basic_binary_op(lxor, bootsXOR)  // r = a ^ b = (a != b)
-    _basic_binary_op(lxnor, bootsXNOR)  // r = !(a ^ b) = (a == b)
-    _basic_binary_op(landyn, bootsANDYN)  // r = a && !b
-    _basic_binary_op(landny, bootsANDNY)  // r = !a && b
-    _basic_binary_op(loryn, bootsORYN)  // r = a || !b
-    _basic_binary_op(lorny, bootsORNY)  // r = !a || b
-    #undef _basic_unary_op
-    #undef _basic_binary_op
-    void lval(LweSample *r, bool a) {  // r = true / false
-        bootsCONSTANT(r, a, this->_keys.get_cloud_key());
-    }
-    void lifelse(LweSample *r, const LweSample *a, const LweSample *b,
-        const LweSample *c) {  // r = a ? b : c
-        bootsMUX(r, a, b, c, this->_keys.get_cloud_key());
-    }
-    // encryption and decryption
-    void encrypt(LweSample *r, bool a) {
-        bootsSymEncrypt(r, a, this->_keys.get_secret_key());
-    }
-    bool decrypt(const LweSample *a) {
-        return bootsSymDecrypt(a, this->_keys.get_secret_key()) != 0;
-    }
-};
+// Key manager
 
-using namespace std;
+EruKey::EruKey() : _secret(nullptr), _cloud(nullptr) {}
 
-int main(int argc, char** argv) {
-    EruEnv client;
-    client.make_keys();
-    auto input = client.create(2);
-    client.lval(input, true);
-    client.lval(input + 1, false);
+EruKey::EruKey(const EruKey &other) : _secret(other._secret),
+    _cloud(other._cloud) {}
 
-    EruEnv server;
-    server.set_keys(client.get_keys());
-    auto res = server.create(1);
-    printf("started.\n");
-    for (int i = 0; i < 10000; i++)
-        server.lor(res, input, input + 1);
-    printf("done.\n");
+EruKey::EruKey(std::shared_ptr<TFheGateBootstrappingSecretKeySet> secret_key,
+    std::shared_ptr<TFheGateBootstrappingCloudKeySet> cloud_key) :
+    _secret(secret_key), _cloud(cloud_key) {}
 
-    bool fn = client.decrypt(res);
-    cout << fn << endl;
-    return 0;
+EruKey EruKey::from_secret_raw(
+        std::shared_ptr<TFheGateBootstrappingSecretKeySet> key) {
+    return EruKey(key, nullptr);
+}
+
+EruKey EruKey::from_cloud_raw(
+        std::shared_ptr<TFheGateBootstrappingCloudKeySet> key) {
+    return EruKey(nullptr, key);
+}
+
+EruKey EruKey::from_secret(EruData key) {
+    std::stringstream stream;
+    stream.write(key.c_str(), key.length());
+    return EruKey::from_secret_raw(
+        std::shared_ptr<TFheGateBootstrappingSecretKeySet>(
+            new_tfheGateBootstrappingSecretKeySet_fromStream(stream),
+            _TFheGateBootstrappingSecretKeySetDeleter()
+        )
+    );
+}
+
+EruKey EruKey::from_cloud(EruData key) {
+    std::stringstream stream;
+    stream.write(key.c_str(), key.length());
+    return EruKey::from_cloud_raw(
+        std::shared_ptr<TFheGateBootstrappingCloudKeySet>(
+            new_tfheGateBootstrappingCloudKeySet_fromStream(stream),
+            _TFheGateBootstrappingCloudKeySetDeleter()
+        )
+    );
+}
+
+const TFheGateBootstrappingSecretKeySet* EruKey::secret_raw() {
+    return _secret.get();
+}
+
+const TFheGateBootstrappingCloudKeySet* EruKey::cloud_raw() {
+    if (_secret != nullptr)
+        return &_secret.get()->cloud;
+    return _cloud.get();
+}
+
+EruData EruKey::secret() {
+    std::stringstream stream;
+    export_tfheGateBootstrappingSecretKeySet_toStream(stream, secret_raw());
+    return _extract_sstream_data(stream);
+}
+
+EruData EruKey::cloud() {
+    std::stringstream stream;
+    export_tfheGateBootstrappingCloudKeySet_toStream(stream, cloud_raw());
+    return _extract_sstream_data(stream);
+}
+
+// Environment base class definitions
+
+template <typename _T>
+_T* EruEnv<_T>::malloc(size_t size) { return nullptr; }
+
+template <typename _T>
+void EruEnv<_T>::mfree(_T* ptr, size_t size) {}
+
+template <typename _T>
+void EruEnv<_T>::lval(_T *r, const bool a) {}
+
+template <typename _T>
+void EruEnv<_T>::ldup(_T *r, const _T *a) {}
+
+template <typename _T>
+void EruEnv<_T>::lnot(_T *r, const _T *a) {}
+
+template <typename _T>
+void EruEnv<_T>::land(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lor(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lnand(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lnor(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lxor(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lxnor(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::landyn(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::landny(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::loryn(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lorny(_T *r, const _T *a, const _T *b) {}
+
+template <typename _T>
+void EruEnv<_T>::lifelse(_T *r, const _T *a, const _T *b, const _T *c) {}
+
+template <typename _T>
+void EruEnv<_T>::encrypt(_T *r, const bool a) {}
+
+template <typename _T>
+bool EruEnv<_T>::decrypt(const _T *a) { return false; }
+
+// Raw environment
+
+bool* EruEnvPlain::malloc(size_t size) {
+    return new bool[size];
+}
+
+void EruEnvPlain::mfree(bool *ptr, size_t size) {
+    delete[] ptr;
+}
+
+void EruEnvPlain::lval(bool *r, const bool a) {
+    *r = a;
+}
+
+void EruEnvPlain::ldup(bool *r, const bool *a) {
+    *r = *a;
+}
+
+void EruEnvPlain::lnot(bool *r, const bool *a) {
+    *r = !*a;
+}
+
+void EruEnvPlain::land(bool *r, const bool *a, const bool *b) {
+    *r = *a && *b;
+}
+
+void EruEnvPlain::lor(bool *r, const bool *a, const bool *b) {
+    *r = *a || *b;
+}
+
+void EruEnvPlain::lnand(bool *r, const bool *a, const bool *b) {
+    *r = !(*a && *b);
+}
+
+void EruEnvPlain::lnor(bool *r, const bool *a, const bool *b) {
+    *r = !(*a || *b);
+}
+
+void EruEnvPlain::lxor(bool *r, const bool *a, const bool *b) {
+    *r = *a ^ *b;
+}
+
+void EruEnvPlain::lxnor(bool *r, const bool *a, const bool *b) {
+    *r = !(*a ^ *b);
+}
+
+void EruEnvPlain::landyn(bool *r, const bool *a, const bool *b) {
+    *r = *a && !*b;
+}
+
+void EruEnvPlain::landny(bool *r, const bool *a, const bool *b) {
+    *r = !*a && *b;
+}
+
+void EruEnvPlain::loryn(bool *r, const bool *a, const bool *b) {
+    *r = *a || !*b;
+}
+
+void EruEnvPlain::lorny(bool *r, const bool *a, const bool *b) {
+    *r = !*a || *b;
+}
+
+void EruEnvPlain::lifelse(bool *r, const bool *a, const bool *b,
+        const bool *c) {
+    *r = *a ? *b : *c;
+}
+
+void EruEnvPlain::encrypt(bool *r, const bool a) {
+    *r = a;
+}
+
+bool EruEnvPlain::decrypt(const bool *a) {
+    return *a;
+}
+
+// Encrypted FHE environment
+
+TFheGateBootstrappingCloudKeySet* EruEnvFhe::_key() {
+    if (_session == nullptr)
+        return nullptr;
+    return const_cast<TFheGateBootstrappingCloudKeySet*>(
+        _session->get_key().cloud_raw()
+    );
+}
+
+EruEnvFhe::EruEnvFhe() : _session(nullptr) {}
+
+EruEnvFhe::EruEnvFhe(EruSession *session) : _session(session) {}
+
+EruEnvFhe::EruEnvFhe(const EruEnvFhe &other) : _session(other._session) {}
+
+EruGate* EruEnvFhe::malloc(size_t size) {
+    auto params = _session->params();
+    return new_gate_bootstrapping_ciphertext_array(size, params);
+}
+
+void EruEnvFhe::mfree(EruGate* ptr, size_t size) {
+    delete_gate_bootstrapping_ciphertext_array(size, ptr);
+}
+
+void EruEnvFhe::lval(EruGate *r, const bool a) {
+    bootsCONSTANT(r, a, _key());
+}
+
+void EruEnvFhe::ldup(EruGate *r, const EruGate *a) {
+    bootsCOPY(r, a, _key());
+}
+
+void EruEnvFhe::lnot(EruGate *r, const EruGate *a) {
+    bootsNOT(r, a, _key());
+}
+
+void EruEnvFhe::land(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsAND(r, a, b, _key());
+}
+
+void EruEnvFhe::lor(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsOR(r, a, b, _key());
+}
+
+void EruEnvFhe::lnand(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsNAND(r, a, b, _key());
+}
+
+void EruEnvFhe::lnor(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsNOR(r, a, b, _key());
+}
+
+void EruEnvFhe::lxor(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsXOR(r, a, b, _key());
+}
+
+void EruEnvFhe::lxnor(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsXNOR(r, a, b, _key());
+}
+
+void EruEnvFhe::landyn(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsANDYN(r, a, b, _key());
+}
+
+void EruEnvFhe::landny(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsANDNY(r, a, b, _key());
+}
+
+void EruEnvFhe::loryn(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsORYN(r, a, b, _key());
+}
+
+void EruEnvFhe::lorny(EruGate *r, const EruGate *a, const EruGate *b) {
+    bootsORNY(r, a, b, _key());
+}
+
+void EruEnvFhe::lifelse(EruGate *r, const EruGate *a, const EruGate *b,
+        const EruGate *c) {
+    bootsMUX(r, a, b, c, _key());
+}
+
+void EruEnvFhe::encrypt(EruGate *r, const bool a) {
+    bootsSymEncrypt(r, a, _session->get_key().secret_raw());
+}
+
+bool EruEnvFhe::decrypt(const EruGate *a) {
+    return bootsSymDecrypt(a, _session->get_key().secret_raw()) != 0;
+}
+
+// Session manager
+
+EruSession::EruSession(int min_lambda) : _min_lambda(min_lambda) {
+    _params = std::shared_ptr<TFheGateBootstrappingParameterSet>(
+        new_default_gate_bootstrapping_parameters(_min_lambda),
+        _TFheGateBootstrappingParameterSetDeleter()
+    );
+    _env = std::shared_ptr<EruEnvFhe>(new EruEnvFhe(this));
+}
+
+EruSession::EruSession(const EruSession &other) :
+    _min_lambda(other._min_lambda), _params(other._params),
+    _key(other._key), _env(other._env) {}
+
+void EruSession::set_seed() {
+    // This function is cryptographically secure
+    int size = _min_lambda * 2 / 32 + 1;
+    auto buffer = std::unique_ptr<uint32_t>(new uint32_t[size]);
+    if (RAND_bytes((unsigned char*)buffer.get(), size * 4) != 1)
+        throw std::runtime_error("cannot generate secure seed");
+    set_seed(buffer.get(), size);
+}
+
+void EruSession::set_seed(uint32_t values[], size_t size) {
+    tfhe_random_generator_setSeed(values, size);
+}
+
+void EruSession::generate_key(bool seed) {
+    if (seed)
+        set_seed();
+    TFheGateBootstrappingSecretKeySet *key;
+    key = new_random_gate_bootstrapping_secret_keyset(_params.get());
+    auto ptr = std::shared_ptr<TFheGateBootstrappingSecretKeySet>(key,
+        _TFheGateBootstrappingSecretKeySetDeleter());
+    _key = EruKey::from_secret_raw(ptr);
+}
+
+void EruSession::set_key(EruKey key) {
+    _key = key;
+}
+
+EruKey EruSession::get_key() {
+    return _key;
+}
+
+TFheGateBootstrappingParameterSet* EruSession::params() {
+    return _params.get();
+}
+
+EruEnvFhe* EruSession::env() {
+    return _env.get();
 }
--- liberu/liberu.h
+++ liberu/liberu.h
@@ -1,10 +1,18 @@
 
+#ifndef _LIBERU_H
+#define _LIBERU_H
+
 #include <tfhe/tfhe.h>
 #include <memory>
 
 
 typedef std::string EruData;
 typedef LweSample EruGate;
+class EruKey;
+template <typename _T> class EruEnv;
+class EruEnvPlain;
+class EruEnvFhe;
+class EruSession;
 
 class EruKey {
 protected:
@@ -16,10 +24,10 @@ public:
     EruKey(const EruKey &other);
     EruKey(std::shared_ptr<TFheGateBootstrappingSecretKeySet> secret_key,
         std::shared_ptr<TFheGateBootstrappingCloudKeySet> cloud_key);
-    static EruKey from_raw_secret(
+    static EruKey from_secret_raw(
         std::shared_ptr<TFheGateBootstrappingSecretKeySet> key);
-    static EruKey from_raw_cloud(
-        std::shared_ptr<TFheGateBootstrappingSecretKeySet> key);
+    static EruKey from_cloud_raw(
+        std::shared_ptr<TFheGateBootstrappingCloudKeySet> key);
     static EruKey from_secret(EruData key);
     static EruKey from_cloud(EruData key);
     // data retrievers
@@ -56,55 +64,36 @@ public:
 
 class EruEnvPlain : public EruEnv<bool> {
 public:
-    bool* malloc(size_t size) {
-        return new bool[size]; }
-    void mfree(bool *ptr, size_t size) {
-        delete[] ptr; }
-    void lval(bool *r, const bool a) {
-        *r = a; }
-    void ldup(bool *r, const bool *a) {
-        *r = *a; }
-    void lnot(bool *r, const bool *a) {
-        *r = !*a; }
-    void land(bool *r, const bool *a, const bool *b) {
-        *r = *a && *b; }
-    void lor(bool *r, const bool *a, const bool *b) {
-        *r = *a || *b; }
-    void lnand(bool *r, const bool *a, const bool *b) {
-        *r = !(*a && *b); }
-    void lnor(bool *r, const bool *a, const bool *b) {
-        *r = !(*a || *b); }
-    void lxor(bool *r, const bool *a, const bool *b) {
-        *r = *a ^ *b; }
-    void lxnor(bool *r, const bool *a, const bool *b) {
-        *r = !(*a ^ *b); }
-    void landyn(bool *r, const bool *a, const bool *b) {
-        *r = *a && !*b; }
-    void landny(bool *r, const bool *a, const bool *b) {
-        *r = !*a && *b; }
-    void loryn(bool *r, const bool *a, const bool *b) {
-        *r = *a || !*b; }
-    void lorny(bool *r, const bool *a, const bool *b) {
-        *r = !*a || *b; }
-    void lifelse(bool *r, const bool *a, const bool *b, const bool *c) {
-        *r = *a ? *b : *c; }
-    void encrypt(bool *r, const bool a) {
-        *r = a; }
-    bool decrypt(const bool *a) {
-        return *a; }
+    bool* malloc(size_t size);
+    void mfree(bool *ptr, size_t size);
+    void lval(bool *r, const bool a);
+    void ldup(bool *r, const bool *a);
+    void lnot(bool *r, const bool *a);
+    void land(bool *r, const bool *a, const bool *b);
+    void lor(bool *r, const bool *a, const bool *b);
+    void lnand(bool *r, const bool *a, const bool *b);
+    void lnor(bool *r, const bool *a, const bool *b);
+    void lxor(bool *r, const bool *a, const bool *b);
+    void lxnor(bool *r, const bool *a, const bool *b);
+    void landyn(bool *r, const bool *a, const bool *b);
+    void landny(bool *r, const bool *a, const bool *b);
+    void loryn(bool *r, const bool *a, const bool *b);
+    void lorny(bool *r, const bool *a, const bool *b);
+    void lifelse(bool *r, const bool *a, const bool *b, const bool *c);
+    void encrypt(bool *r, const bool a);
+    bool decrypt(const bool *a);
 };
 
 class EruEnvFhe : public EruEnv<EruGate> {
-protected:
+private:
     EruSession *_session;
-    TFheGateBootstrappingCloudKeySet *_key_cache;
     TFheGateBootstrappingCloudKeySet* _key();
 public:
     EruEnvFhe();
     EruEnvFhe(EruSession *session);
     EruEnvFhe(const EruEnvFhe &other);
     EruGate* malloc(size_t size);
-    void mfree(EruGate* ptr, size_t size);
+    void mfree(EruGate *ptr, size_t size);
     void lval(EruGate *r, const bool a);
     void ldup(EruGate *r, const EruGate *a);
     void lnot(EruGate *r, const EruGate *a);
@@ -118,8 +107,7 @@ public:
     void landny(EruGate *r, const EruGate *a, const EruGate *b);
     void loryn(EruGate *r, const EruGate *a, const EruGate *b);
     void lorny(EruGate *r, const EruGate *a, const EruGate *b);
-    void lifelse(EruGate *r, const EruGate *a, const EruGate *b,
-        const EruGate *c);
+    void lifelse(EruGate *r, const EruGate *a, const EruGate *b, const EruGate *c);
     void encrypt(EruGate *r, const bool a);
     bool decrypt(const EruGate *a);
 };
@@ -129,15 +117,23 @@ protected:
     int _min_lambda;
     std::shared_ptr<TFheGateBootstrappingParameterSet> _params;
     EruKey _key;
+    std::shared_ptr<EruEnvFhe> _env;
 public:
     EruSession(int min_lambda);
     EruSession(const EruSession &other);
     // Reset session seed
     void set_seed();
+    void set_seed(uint32_t values[], size_t size);
     // Generate new keypair
-    void generate_key();
+    void generate_key(bool seed = true);
     // Set existing key
     void set_key(EruKey key);
     // Get key
     EruKey get_key();
+    // Get current parameters
+    TFheGateBootstrappingParameterSet* params();
+    // Get session environment
+    EruEnvFhe* env();
 };
+
+#endif  // _LIBERU_H
