b4de9e01d30c9460d85e36736f4e308888cfa4a6
Geoffrey Tang <jeffswt@users.noreply.github.com>
2021-06-25 15:12:32 +0800

    feat: add userspace context wrapper

---
+++ liberu/liberu_context.cpp
@@ -0,0 +1,91 @@
+
+// liberu_context.cpp: context exposing to users
+// MIT License
+//
+// Copyright (c) 2021 Geoffrey Tang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include "liberu_context.h"
+
+
+template <typename _T>
+EruContext<_T>::EruContext(int min_lambda) {
+    __session = nullptr;
+    __allocator = std::unique_ptr<EruAllocator<_T>>(new EruAllocator<_T>(
+        16, nullptr));
+    __env = std::unique_ptr<EruEnv<_T>>((EruEnv<_T>*)new EruEnvPlain());
+}
+template <>
+EruContext<EruGate>::EruContext(int min_lambda) {
+    __session = std::unique_ptr<EruSession>(new EruSession(min_lambda));
+    __allocator = std::unique_ptr<EruAllocator<EruGate>>(
+        new EruAllocator<EruGate>(16, __session.get()->params())
+    );
+    __env = nullptr;
+}
+
+template <typename _T>
+EruSession* EruContext<_T>::_session() {
+    return __session.get();
+}
+
+template <typename _T>
+EruAllocator<_T>* EruContext<_T>::_allocator() {
+    return __allocator.get();
+}
+
+template <typename _T>
+EruEnv<_T>* EruContext<_T>::_env() {
+    if (__session != nullptr)
+        return (EruEnv<_T>*)__session.get()->env();
+    return __env.get();
+}
+
+template <typename _T>
+void EruContext<_T>::gen_secret_key() {
+    if (__session != nullptr)
+        __session.get()->generate_key();
+}
+
+template <typename _T>
+void EruContext<_T>::set_secret_key(EruData key) {
+    if (__session != nullptr)
+        __session.get()->set_key(EruKey::from_secret(key));
+}
+
+template <typename _T>
+void EruContext<_T>::set_cloud_key(EruData key) {
+    if (__session != nullptr)
+        __session.get()->set_key(EruKey::from_cloud(key));
+}
+
+template <typename _T>
+EruData EruContext<_T>::get_secret_key() {
+    if (__session != nullptr)
+        return __session.get()->get_key().secret();
+    return "";
+}
+
+template <typename _T>
+EruData EruContext<_T>::get_cloud_key() {
+    if (__session != nullptr)
+        return __session.get()->get_key().cloud();
+    return "";
+}
--- liberu/liberu_context.h
+++ liberu/liberu_context.h
@@ -1,5 +1,5 @@
 
-// liberu.h: main header
+// liberu_context.h: context exposing to users
 // MIT License
 //
 // Copyright (c) 2021 Geoffrey Tang
@@ -22,11 +22,31 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-#ifndef _LIBERU_H
-#define _LIBERU_H
+#ifndef _LIBERU_CONTEXT_H
+#define _LIBERU_CONTEXT_H
 
 #include "liberu_base.h"
 #include "liberu_alloc.h"
-#include "liberu_context.h"
 
-#endif  // _LIBERU_H
+template <typename _T>
+class EruContext {
+private:
+    std::unique_ptr<EruSession> __session;
+    std::unique_ptr<EruAllocator<_T>> __allocator;
+    std::unique_ptr<EruEnv<_T>> __env;  // when __session is unavailable
+public:
+    EruContext(int min_lambda);
+    // Medium-level interfaces that you really shouldn't touch
+    // unless you know what you're doing
+    EruSession* _session();
+    EruAllocator<_T>* _allocator();
+    EruEnv<_T>* _env();
+    // Key management
+    void gen_secret_key();
+    void set_secret_key(EruData key);
+    void set_cloud_key(EruData key);
+    EruData get_secret_key();
+    EruData get_cloud_key();
+};
+
+#endif  // _LIBERU_CONTEXT_H
