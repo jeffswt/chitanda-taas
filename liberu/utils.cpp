b895012ef66304a230ab64778cd7a30851fa0545
Geoffrey Tang <jeffswt@users.noreply.github.com>
2021-06-30 20:29:00 +0800

    feat: add string list en-decoder

--- liberu/utils.cpp
+++ liberu/utils.cpp
@@ -9,3 +9,40 @@ EruData _EruHazmat::dump_sstream(std::stringstream &stream) {
         result += (char)ch;
     return result;
 }
+
+/// binobjlist := <null> // <binobjlist> <binobj>
+/// binobj := <length> <data>, whereas len(data) == length
+/// data := any binary string
+/// length := [0xff] // <num> <length>, nums are stored in little-endian
+/// num := [0x00] .. [0xfe], stored in base-255
+EruData binobjlist_encode(const std::vector<EruData>& objs) {
+    EruData result;
+    for (auto &obj : objs) {
+        // encode length
+        uint64_t len = obj.length();
+        for (; len > 0; len /= 255)
+            result += (char)(len % 255);
+        result += (char)0xff;
+        // add data
+        result += obj;
+    }
+    return result;
+}
+
+std::vector<EruData> binobjlist_decode(const EruData &data) {
+    std::vector<EruData> result;
+    EruData buffer;
+    for (int i = 0; i < data.length(); ) {
+        uint64_t len = 0, pwr = 1;
+        for (; data[i] != 0xff; i++) {
+            len += (uint64_t)data[i] * pwr;
+            pwr *= 255;
+        }
+        i++;
+        for (int j = 0; j < len; i++, j++)
+            buffer += result[i];
+        result.push_back(buffer);
+        buffer.clear();
+    }
+    return result;
+}
--- liberu/utils.h
+++ liberu/utils.h
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 
 typedef std::string EruData;
@@ -14,6 +15,16 @@ namespace _EruHazmat {
     /// @param stream: String stream as input.
     /// @return Binary string.
     EruData dump_sstream(std::stringstream &stream);
+
+    /// Stack multiple binary strings into one.
+    /// @param objs: A vector of multiple objects.
+    /// @return: Decodable single string.
+    EruData binobjlist_encode(const std::vector<EruData> &objs);
+
+    /// Strip encoded binary list object bulk into multiple objects.
+    /// @param data: binobjlist_encode'd object aggregate.
+    /// @return: List of EruData's.
+    std::vector<EruData> binobjlist_decode(const EruData &data);
 }
 
 #endif  // _LIBERU_UTILS_H
